263a9b92cd782adc059f59fa65c9fd3e388012c1
predYz <83245515+predYz@users.noreply.github.com>
2022-07-22 09:06:30 +0200

    Added 2nd container
    Added 2nd container with margin, padding, text and stlyes

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,72 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'predYz',
-                style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold, fontFamily: 'Pacifico'),
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'
+                ),
               ),
               Text(
                 'CEO OF PredYz CORPORATON',
-                style: TextStyle(letterSpacing: 2.5, fontSize: 20.0, color: Colors.teal.shade100, fontWeight: FontWeight.bold, fontFamily: 'SourceSansPro'),
+                style: TextStyle(
+                    letterSpacing: 2.5,
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro'
+                ),
               ),
               Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0
+                ),
                 child: Row(
                   children: <Widget>[
-                    Icon(icon)
+                    Icon(
+                        Icons.phone,
+                        size: 20.0,
+                        color: Colors.teal
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+36 30 757 30 40',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'bagdia92@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                      ),
+                    )
                   ],
                 ),
               )
